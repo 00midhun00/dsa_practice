c8b47fc1187ebb7a174986d72998874123063347
root <root@X.>
2025-01-02 09:43:10 +0000

    que

---
+++ que.c
@@ -0,0 +1,102 @@
+#include<stdio.h>   
+#include<stdlib.h>  
+#define maxsize 5  
+void enque();  
+void deque();  
+void display();  
+int front = -1, rear = -1;  
+int queue[maxsize];  
+void main ()  
+{  
+    int choice;   
+    while(choice != 4)   
+    {     
+        printf("\n*************************Main Menu*****************************\n");  
+        printf("\n=================================================================\n");  
+        printf("\n1.enque\n2.deque\n3.display\n4.Exit\n");  
+        printf("\nEnter your choice ?");  
+        scanf("%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            enque();  
+            break;  
+            case 2:  
+            deque();  
+            break;  
+            case 3:  
+            display();  
+            break;  
+            case 4:  
+            exit(0);  
+            break;  
+            default:   
+            printf("\nEnter valid choice??\n");  
+        }  
+    }  
+}  
+void enque()  
+{  
+    int item;  
+    printf("\nEnter the element\n");  
+    scanf("\n%d",&item);      
+    if(rear == maxsize-1)  
+    {  
+        printf("\nOVERFLOW\n");  
+        return;  
+    }  
+    if(front == -1 && rear == -1)  
+    {  
+        front = 0;  
+        rear = 0;  
+    }  
+    else   
+    {  
+        rear = rear+1;  
+    }  
+    queue[rear] = item;  
+    printf("\nValue inserted ");  
+      
+}  
+void deque()  
+{  
+    int item;   
+    if (front == -1 || front > rear)  
+    {  
+        printf("\nUNDERFLOW\n");  
+        return;  
+              
+    }  
+    else  
+    {  
+        item = queue[front];  
+        if(front == rear)  
+        {  
+            front = -1;  
+            rear = -1 ;  
+        }  
+        else   
+        {  
+            front = front + 1;  
+        }  
+        printf("\nvalue deleted ");  
+    }  
+      
+      
+}  
+      
+void display()  
+{  
+    int i;  
+    if(rear == -1)  
+    {  
+        printf("\nEmpty queue\n");  
+    }  
+    else  
+    {   printf("\nprinting values .....\n");  
+        for(i=front;i<=rear;i++)  
+        {  
+            printf("\n%d\n",queue[i]);  
+        }     
+    }  
+}  
